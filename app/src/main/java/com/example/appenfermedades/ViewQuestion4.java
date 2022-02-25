5f460c1bb4143353b148c2e824c1cea5492a4849
jpceci <cecilia_jp@live.com>
2022-02-25 12:27:55 -0400

    ViewQuestion2-4, viewEscala2-4 creados.

--- app/src/main/java/com/example/appenfermedades/ViewQuestion4.java
+++ app/src/main/java/com/example/appenfermedades/ViewQuestion4.java
@@ -6,18 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class ViewQuestion1 extends AppCompatActivity {
+public class ViewQuestion4 extends AppCompatActivity {
+
     Button btnSiguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_question1);
-        btnSiguiente = findViewById(R.id.btnSiguienteEscala1);
-        btnSiguiente.setOnClickListener(view -> Pantalla_escala1());
+        btnSiguiente = findViewById(R.id.btnSiguienteEscala4);
+        btnSiguiente.setOnClickListener(view -> Pantalla_escala4());
 
     }
-    public void Pantalla_escala1() {
-        Intent intent = new Intent(this, ViewEscala1.class);
+    public void Pantalla_escala4() {
+        Intent intent = new Intent(this, ViewEscala4.class);
         startActivity(intent);
     }
 }
