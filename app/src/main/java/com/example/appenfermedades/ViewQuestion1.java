3d71a137bc41b7112c80bc70ec783a222966de96
Bianca Romero Guzman <bianca.09012001@gmail.com>
2022-03-04 17:51:58 -0400

    planilla solo de preguntas lista

--- app/src/main/java/com/example/appenfermedades/ViewQuestion1.java
+++ app/src/main/java/com/example/appenfermedades/ViewQuestion1.java
@@ -1,10 +1,9 @@
 package com.example.appenfermedades;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewQuestion1 extends AppCompatActivity {
     Button btnSiguiente;
@@ -13,11 +12,11 @@ public class ViewQuestion1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_question1);
         btnSiguiente = findViewById(R.id.btnSiguienteEscala1);
-        btnSiguiente.setOnClickListener(view -> Pantalla_escala1());
+//      btnSiguiente.setOnClickListener(view -> Pantalla_escala1());
 
     }
-    public void Pantalla_escala1() {
-        Intent intent = new Intent(this, ViewEscala1.class);
-        startActivity(intent);
-    }
+//    public void Pantalla_escala1() {
+//        Intent intent = new Intent(this, ViewEscala1.class);
+//        startActivity(intent);
+//    }
 }
